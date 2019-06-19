e92d93c88b13dfd678d36a61107f2f4e7395cf6a
Ubuntu <ubuntu@ip-172-31-89-196.ec2.internal>
2019-06-19 10:39:58 +0000

    Google maps API added. Drafting search box ideas/options - in progress

---
+++ assets/js/maps.js
@@ -0,0 +1,12 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        mapTypeId: 'satellite',
+        zoom: 2,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+
+}
+
