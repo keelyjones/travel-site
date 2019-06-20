14eda2ccce8dbec1cf17d6ffbd51609f29bac210
Ubuntu <ubuntu@ip-172-31-89-196.ec2.internal>
2019-06-20 10:01:09 +0000

    Added geolocation, changed map type to roadview, zoom to 2.5, default lat/long to North Atlantic Ocean. Added jQuery CDN

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,12 +1,42 @@
+var map, infoWindow;
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        mapTypeId: 'satellite',
-        zoom: 2,
-        center: {
-            lat: 46.619261,
-            lng: -33.134766
-        }
+        mapTypeId: 'roadmap',
+        zoom: 2.5,
+        center: {lat: 40.518385783899, lng: -40.518385783899},
     });
+    
+     infoWindow = new google.maps.InfoWindow;
 
-}
+// Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
 
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('You are here');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+      
+// Set user geolocation/send error message.      
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+       
+
+      
